148dcc59b420f1b5c31e0fd391c789c9dd5f76f1
Mminzy22 <149351897+Mminzy22@users.noreply.github.com>
2024-12-12 09:32:46 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 2.43 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ Python3/프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -3,8 +3,7 @@ def solution(s):
     while '1' != s:
         zeros += s.count('0')
         s = s.replace('0','')
-        s = bin(len(s))
-        s = s[2:]
+        s = (bin(len(s)))[2:]
         count += 1
         
     answer = [count,zeros]
