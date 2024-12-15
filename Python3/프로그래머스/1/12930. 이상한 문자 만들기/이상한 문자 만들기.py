7331e723df914f5e3acca12a3d3d61fb70b57335
Mminzy22 <149351897+Mminzy22@users.noreply.github.com>
2024-12-15 15:28:56 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,14 @@
+def solution(s):
+    words = s.split(' ')
+    result = []
+    
+    for word in words:
+        new_word = ''
+        for i, char in enumerate(word):
+            if i % 2 == 0:
+                new_word += char.upper()
+            else:
+                new_word += char.lower()
+        result.append(new_word)
+    
+    return ' '.join(result)
