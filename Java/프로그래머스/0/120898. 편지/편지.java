3adae0bb85c1e7b81e663e0b515de480f880e903
박민지 <149351897+Mminzy22@users.noreply.github.com>
2025-08-27 23:11:12 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        
+        int strSize = message.length();
+        answer = strSize*2;
+        
+        return answer;
+    }
+}
